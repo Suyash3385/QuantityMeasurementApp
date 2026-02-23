d4f69b87ca7ba4e03c198431c8cbe6d60c9a8961
Suyash <suyashsharma942@gmail.com>
2026-02-23 22:37:01 +0530

    [Suyash]:Added UC11

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -33,5 +33,15 @@ public class QuantityMeasurementApp {
         // addition
         Quantity<WeightUnit> addedWeight = weight1.add(weight2);
         System.out.println("1 Kg + 1000 g in Kg : " + addedWeight.getValue());
+        
+        // VolumeUnit
+        Quantity<VolumeUnit> v1 = new Quantity<>(1.0, VolumeUnit.LITRE);
+        Quantity<VolumeUnit> v2 = new Quantity<>(1000.0, VolumeUnit.MILLILITRE);
+        Quantity<VolumeUnit> v3 = new Quantity<>(1.0, VolumeUnit.GALLON);
+
+        System.out.println(v1.equals(v2)); 
+        System.out.println(v1.convertTo(VolumeUnit.MILLILITRE)); 
+        System.out.println(v1.add(v2)); 
+        System.out.println(v1.add(v3, VolumeUnit.MILLILITRE)); 
     }
 }
