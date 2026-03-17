24cd2829e0d44f17ed258961d6b605bb7a6c2ada
Suyash <suyashsharma942@gmail.com>
2026-03-17 12:25:20 +0530

    [Suyash]:Added UC16 Database Integration

---
+++ src/main/java/com/app/exception/QuantityMeasurementException.java
@@ -0,0 +1,12 @@
+package com.app.exception;
+
+public class QuantityMeasurementException extends RuntimeException{
+	public static final long serialVersionUID=1L;
+
+	public QuantityMeasurementException(String str) {
+		super(str);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
